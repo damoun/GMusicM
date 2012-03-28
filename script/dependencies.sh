096b766244517e2b21e8342c0e69aeda397755fe
Damoun <damoun60@gmail.com>
2012-03-28 16:07:19 +0200

    add gmusicapi dependence

--- script/dependencies.sh
+++ script/dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo apt-get install 'python-setuptools'
-sudo easy_install 'decorator' 'mutagen' 'protobuf' 'mechanize' 'validictory>=8.1'
+sudo easy_install 'decorator' 'mutagen' 'protobuf' 'mechanize' 'validictory>=8.1' 'gmusicapi'
