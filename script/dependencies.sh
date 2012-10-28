b1d132237e273b2a26e8b7439ee6cd4c7dc2e162
damoun <damoun60@gmail.com>
2012-10-28 22:55:27 +0800

    updated dependancies

--- script/dependencies.sh
+++ script/dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo apt-get install 'python-setuptools'
-sudo easy_install 'decorator' 'mutagen' 'protobuf' 'mechanize' 'validictory>=8.1' 'gmusicapi'
+sudo easy_install 'decorator' 'mutagen' 'protobuf' 'mechanize' 'validictory' 'gmusicapi'
